6fc84f9b93dc0acc638e6d66baff0bea9126305a
johnie261 <njorogejohn261@gmail.com>
2023-03-24 02:25:52 +0300

    Update app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Calculator from './components/calculator';
 import './App.css';
+import Quote from './components/quote';
 
 const App = () => (
   <div className="calcu">
     <Calculator />
+    <Quote />
   </div>
 );
 export default App;
